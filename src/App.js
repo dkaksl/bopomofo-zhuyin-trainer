067a86fc6d52832dbe1a6440463c61bed6b67e22
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-23 19:27:37 +0200

    added input and handler method that triggers update of random character; fixed imports

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { useState } from 'react'
 import { hot } from 'react-hot-loader'
 
-import { characterDict } from './Dictionary'
-
-function getRandomCharacterFromDict() {
-  return characterDict[Math.floor(Math.random() * characterDict.length)]
-}
+import {
+  getRandomCharacterFromDict,
+  getRandomCharacterFromDictExcept,
+} from './Dictionary'
 
 function App() {
   const [randomCharacter, setRandomCharacter] = useState({})
 
+  function handleKeyInput(event) {
+    var input = event.target.value
+    event.target.value = ''
+    if (input === randomCharacter.key) {
+      setRandomCharacter(getRandomCharacterFromDictExcept(randomCharacter))
+    }
+  }
+
   return (
     <main>
-      <h1>ㄅㄆㄇㄈ/注音 Trainer</h1>
+      <h1>ㄅㄆㄇㄈ／注音 Trainer</h1>
       <button onClick={() => setRandomCharacter(getRandomCharacterFromDict())}>
         Get random character
       </button>
       <p>Random character: {randomCharacter.character}</p>
       <p>Reading: {randomCharacter.reading}</p>
       <p>Key: {randomCharacter.key}</p>
+
+      <input onChange={(event) => handleKeyInput(event)}></input>
     </main>
   )
 }
