e202450abe6970106ec2bd6a2b0c859ceab917eb
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-23 15:08:35 +0200

    added key info to character dict

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ const characterDict = [
     id: 1,
     character: 'ㄅ',
     reading: 'b',
+    key: '1',
   },
   {
     id: 2,
     character: 'ㄆ',
     reading: 'p',
+    key: 'q',
   },
   {
     id: 3,
     character: 'ㄇ',
     reading: 'm',
+    key: 'a',
   },
   {
     id: 4,
     character: 'ㄈ',
     reading: 'f',
+    key: 'z',
   },
 ]
 
