d2415dd196703cbf303c1ccbba555f8d7961c357
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-23 16:45:25 +0200

    added rendering of key

--- src/App.js
+++ src/App.js
@@ -3,45 +3,6 @@ import { hot } from 'react-hot-loader'
 
 import { characterDict } from './Dictionary'
 
-/*
- '2': 'ㄉ',
- 'w': 'ㄊ',
- 's': 'ㄋ',
- 'x': 'ㄌ',
- '3': 'ˇ',
- 'e': 'ㄍ',
- 'd': 'ㄎ',
- 'c': 'ㄏ',
- '4': 'ˋ',
- 'r': 'ㄐ',
- 'f': 'ㄑ',
- 'v': 'ㄒ',
- '5': 'ㄓ',
- 't': 'ㄔ',
- 'g': 'ㄕ',
- 'b': 'ㄖ',
- '6': 'ˊ',
- 'y': 'ㄗ',
- 'h': 'ㄘ',
- 'n': 'ㄙ',
- '7': '˙',
- 'u': 'ㄧ',
- 'j': 'ㄨ',
- 'm': 'ㄩ',
- '8': 'ㄚ',
- 'i': 'ㄛ',
- 'k': 'ㄜ',
- '9': 'ㄞ',
- 'o': 'ㄟ',
- 'l': 'ㄠ',
- '.': 'ㄡ',
- '0': 'ㄢ',
- 'p': 'ㄣ',
- ';': 'ㄤ',
- '/': 'ㄥ',
- '-': 'ㄦ'
- */
-
 function getRandomCharacterFromDict() {
   return characterDict[Math.floor(Math.random() * characterDict.length)]
 }
@@ -57,6 +18,7 @@ function App() {
       </button>
       <p>Random character: {randomCharacter.character}</p>
       <p>Reading: {randomCharacter.reading}</p>
+      <p>Key: {randomCharacter.key}</p>
     </main>
   )
 }
