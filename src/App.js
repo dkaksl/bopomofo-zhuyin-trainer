13624d2d16760df1843a6b037913e0ba4027a23a
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-24 13:45:11 +0200

    implemented use of new component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { hot } from 'react-hot-loader'
-
-import {
-  getRandomCharacterFromDict,
-  getRandomCharacterFromDictExcept,
-} from './Dictionary'
+import Flashcard from './components/Flashcard'
 
 function App() {
-  useEffect(() => {
-    setRandomCharacter(getRandomCharacterFromDict())
-  }, [])
-
-  const [randomCharacter, setRandomCharacter] = useState({})
-
-  function handleKeyInput(event) {
-    var input = event.target.value
-    event.target.value = ''
-    if (input === randomCharacter.key) {
-      setRandomCharacter(getRandomCharacterFromDictExcept(randomCharacter))
-    }
-  }
-
   return (
     <main>
       <h1>ㄅㄆㄇㄈ／注音 Trainer</h1>
-      <button onClick={() => setRandomCharacter(getRandomCharacterFromDict())}>
-        Get random character
-      </button>
-      <p>Random character: {randomCharacter.character}</p>
-      <p>Reading: {randomCharacter.reading}</p>
-      <p>Key: {randomCharacter.key}</p>
-
-      <label htmlFor="keyInput">
-        Input the key corresponding to {randomCharacter.character}:
-      </label>
-      <input id="keyInput" onChange={(event) => handleKeyInput(event)}></input>
+      <Flashcard />
     </main>
   )
 }
