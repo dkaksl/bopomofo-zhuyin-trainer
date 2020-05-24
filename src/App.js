45e872571756b47491ccb5552942ae82fb3b4e12
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-24 16:48:46 +0200

    added buttons for toggling between input and binary flashcards modes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { hot } from 'react-hot-loader'
 import Flashcard from './components/Flashcard'
 import BinaryChoiceFlashcard from './components/BinaryChoiceFlashcard'
 
 function App() {
+  const [mode, setMode] = useState('')
+
   return (
     <main className="main">
       <h1>ㄅㄆㄇㄈ／注音 Trainer</h1>
-      <Flashcard />
-      <BinaryChoiceFlashcard />
+      <button onClick={() => setMode('binary-flash')}>Reading Trainer</button>
+      <button onClick={() => setMode('input')}>Typing Trainer</button>
+      {mode === 'input' ? <Flashcard /> : <BinaryChoiceFlashcard />}
       <footer>
         {new Date().getFullYear()} &copy;{' '}
         <a
