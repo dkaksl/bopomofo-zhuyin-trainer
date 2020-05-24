d736974d73cbdd67c90b503623f72072c60a271b
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-24 14:13:18 +0200

    added binary choice component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import Flashcard from './components/Flashcard'
+import BinaryChoiceFlashcard from './components/BinaryChoiceFlashcard'
 
 function App() {
   return (
     <main>
       <h1>ㄅㄆㄇㄈ／注音 Trainer</h1>
       <Flashcard />
+      <BinaryChoiceFlashcard />
     </main>
   )
 }
