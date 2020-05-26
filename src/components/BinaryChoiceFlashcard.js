58d519ba23e6b62164b9971240144952c7f8234b
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-27 00:02:55 +0200

    implemented modal in binary flashcard component

--- src/components/BinaryChoiceFlashcard.js
+++ src/components/BinaryChoiceFlashcard.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import HelperModal from './HelperModal'
 
 import {
   getRandomCharacterFromDict,
   getRandomCharacterFromDictExcept,
 } from './Dictionary'
+
+import { binaryFlashcardHelp } from './HelperText'
 
 function BinaryChoiceFlashcard() {
   useEffect(() => {
@@ -22,6 +25,7 @@ function BinaryChoiceFlashcard() {
   const [randomCharacter, setRandomCharacter] = useState({})
   const [wrongCharacter, setWrongCharacter] = useState({})
   const [heads, setHeads] = useState(false)
+  const [modalActive, setModalActive] = useState(false)
 
   function handleButtonPress(character) {
     if (character === randomCharacter) {
@@ -30,25 +34,37 @@ function BinaryChoiceFlashcard() {
   }
 
   return (
-    <div className="binary-flashcard-container">
-      <div className="random-character">{randomCharacter.character}</div>
-      <div className="guess-button-container">
-        <button
-          className="guess-button"
-          onClick={() =>
-            handleButtonPress(heads ? randomCharacter : wrongCharacter)
-          }
-        >
-          {heads ? randomCharacter.reading : wrongCharacter.reading}
+    <div>
+      <HelperModal
+        text={binaryFlashcardHelp}
+        active={modalActive}
+        setActive={setModalActive}
+      />
+      <div className="flash-card">
+        <button className="modal-button" onClick={() => setModalActive(true)}>
+          ？
         </button>
-        <button
-          className="guess-button"
-          onClick={() =>
-            handleButtonPress(!heads ? randomCharacter : wrongCharacter)
-          }
-        >
-          {!heads ? randomCharacter.reading : wrongCharacter.reading}
-        </button>
+        <div className="binary-flashcard-container">
+          <div className="random-character">{randomCharacter.character}</div>
+          <div className="guess-button-container">
+            <button
+              className="guess-button"
+              onClick={() =>
+                handleButtonPress(heads ? randomCharacter : wrongCharacter)
+              }
+            >
+              {heads ? randomCharacter.reading : wrongCharacter.reading}
+            </button>
+            <button
+              className="guess-button"
+              onClick={() =>
+                handleButtonPress(!heads ? randomCharacter : wrongCharacter)
+              }
+            >
+              {!heads ? randomCharacter.reading : wrongCharacter.reading}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
