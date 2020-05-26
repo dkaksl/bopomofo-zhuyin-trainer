6cc784072c5161b8eec0804f4359338201e0a434
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-27 00:01:36 +0200

    added collection of modal content

---
+++ src/components/HelperText.js
@@ -0,0 +1,11 @@
+export const binaryFlashcardHelp = {
+  header: 'How to Play',
+  content:
+    'Select the reading that matches the character on the screen by clicking it!',
+}
+
+export const inputFlashcardHelp = {
+  header: 'How to Play',
+  content:
+    'Press the key on your keyboard you would need to press to type the character!',
+}
